03c9392e2455128041ca1b5568edf776f9e25e04
luvak4 <luvak4@gmail.com>
2016-10-03 12:40:03 +0200

    Update arduino-domotico-digitanalog.ino

--- arduino-domotico-digitanalog.ino
+++ arduino-domotico-digitanalog.ino
@@ -1,67 +1,63 @@
 #include <VirtualWire.h>
 const int led_pin = 2;
-const int transmit_pin = 12;
 const int receive_pin = 11;
+const int transmit_pin = 12;
 // max lenght of my message
 const int MSG_LEN = 7;
-// sensore analogico
+// analogic port
 const int sensorPin = A0;
+////////////////////////////////
+// setup
+////////////////////////////////
 void setup() {
-  // LEDs
-  pinMode(led_pin, OUTPUT);
-  // radio tx rx
-  vw_set_tx_pin(transmit_pin);
-  vw_set_rx_pin(receive_pin);  
-  vw_setup(2000);      
-  vw_rx_start(); 
-  //  Serial.begin(9600);
-  // SPEGNE PIN 13 ACCESO DI DEFAULT
-  pinMode(13,OUTPUT);
-  digitalWrite(13,LOW);
+  pinMode(led_pin, OUTPUT);    // led set pin
+  vw_set_tx_pin(transmit_pin); // radio set tx pin
+  vw_set_rx_pin(receive_pin);  // radio set rx pin
+  vw_setup(2000);              // radio speed
+  vw_rx_start();               // radio rx ON
+  pinMode(13,OUTPUT);          // out pin 13
+  digitalWrite(13,LOW);        // ... set low
 }
 
+////////////////////////////////
+// loop
+////////////////////////////////
 void loop(){
-    uint8_t buf[MSG_LEN]={0,0,0,0,0,0,0};
-    uint8_t buflen = MSG_LEN;  
-    if (vw_get_message(buf, &buflen)){
-      vw_rx_stop(); 
-      if (buf[0]==0xAA){
-        switch (buf[1]){
-          case 0x07:digitalWrite(led_pin,HIGH);break;
-          case 0x08:digitalWrite(led_pin,LOW);break;
-          case 0x09:txStatoRele();break;
-          case 0x0A:txAnalogicoA0();break;
-        }
+  uint8_t buf[MSG_LEN]={0,0,0,0,0,0,0}; // empty buffer
+  uint8_t buflen = MSG_LEN;             // lenght of buffer
+  if (vw_get_message(buf, &buflen)){    // received a message?
+    vw_rx_stop();                       // stop radio rx
+    if (buf[0]==0xAA){
+      switch (buf[1]){
+      case 0x07:digitalWrite(led_pin,HIGH);break;
+      case 0x08:digitalWrite(led_pin,LOW);break;
+      case 0x09:txStatoRele();break;
+      case 0x0A:txAnalogicoA0();break;
       }
-      vw_rx_start();
     }
+    vw_rx_start();                      // restart radio rx
+  }
 }
-
-
 
 void txStatoRele(){
-  // acceso
-  char msg[MSG_LEN] = {0xAD,0x01,0,0,0,0,0};
+  char msg[MSG_LEN] = {0xAD,0x01,0,0,0,0,0}; // set buffer base-value
   if (!digitalRead(led_pin)){
-    // spento
-    msg[1]=0x02;
+    msg[1]=0x02;                   // overwrite 2nd byte
   }
-  delay(100);
-  vw_send((uint8_t *)msg,MSG_LEN);
-  vw_wait_tx(); // Wait until the whole message is gone  
+  delay(100);                      // delay
+  vw_send((uint8_t *)msg,MSG_LEN); // send to tx-radio
+  vw_wait_tx();                    // wait until message is gone
 }
 
 void txAnalogicoA0(){
-  char temp[3];
-  char msg[MSG_LEN] = {0xAD,0x03,0,0,0,0,0};
-  int sensorValue = analogRead(sensorPin);
-  
-  itoa(sensorValue, temp, 10);
-  
-  msg[2]=temp[0];
-  msg[3]=temp[1];
-  msg[4]=temp[2];  
-  delay(100);
-  vw_send((uint8_t *)msg,MSG_LEN);
-  vw_wait_tx(); // Wait until the whole message is gone  
+  char temp[3];                    
+  char msg[MSG_LEN] = {0xAD,0x03,0,0,0,0,0}; // set buffer base-value
+  int sensorValue = analogRead(sensorPin);   // read A0
+  itoa(sensorValue, temp, 10);  // int to char-array
+  msg[2]=temp[0];               // overwrite 3rd byte
+  msg[3]=temp[1];               // overwrite 4th byte
+  msg[4]=temp[2];               // overwrite 5th byte
+  delay(100);                   // delay
+  vw_send((uint8_t *)msg,MSG_LEN); // send to tx radio
+  vw_wait_tx();                    // wait until message is gone  
 }
