0c72319e184c203b0b2e44cd1f77dd5579ba0689
luvak4 <luvak4@gmail.com>
2016-10-05 11:47:14 +0200

    Update arduino-domotico-digitanalog.ino

--- arduino-domotico-digitanalog.ino
+++ arduino-domotico-digitanalog.ino
@@ -27,44 +27,54 @@ void loop(){
   uint8_t buf[MSG_LEN]={0,0,0,0,0,0,0}; // empty buffer
   uint8_t buflen = MSG_LEN;             // lenght of buffer
   if (vw_get_message(buf, &buflen)){    // received a message?
-    digitalWrite(13,HIGH);    
     if (buf[0]==0xAA){
+      digitalWrite(13,HIGH);    
       switch (buf[1]){
-      case 0x07:digitalWrite(led_pin,HIGH);break;
-      case 0x08:digitalWrite(led_pin,LOW);break;
-      case 0x09:vw_rx_stop();delay(1000);txStatoRele();vw_rx_start();break;
-      case 0x0A:vw_rx_stop();delay(1000);txAnalogicoA0();vw_rx_start();break;
+      case 0x07:digitalWrite(led_pin,HIGH);txStatoRele();break;
+      case 0x08:digitalWrite(led_pin,LOW);txStatoRele();break;
+      case 0x09:txStatoRele();break;
+      case 0x0A:txAnalogicoA0();break;
+      case 0x0B:digitalWrite(led_pin,!digitalRead(led_pin));txStatoRele();break;
       }
+      digitalWrite(13,LOW);    
     }
   }
   if ((millis()-millPrec)>5000){   
     millPrec=millis();
-    vw_rx_stop();
     txAnalogicoA0();
-    delay(200);
-    vw_rx_start();
   }
 }
 
 void txStatoRele(){
   char msg[MSG_LEN] = {0xAD,0x01,0,0,0,0,0}; // set buffer base-value
+  vw_rx_stop();
+  delayForRadioRxAdj();
   if (!digitalRead(led_pin)){
     msg[1]=0x02;                   // overwrite 2nd byte
   }
-  delay(100);                      // delay
   vw_send((uint8_t *)msg,MSG_LEN); // send to tx-radio
   vw_wait_tx();                    // wait until message is gone
+  vw_rx_start();
 }
 
 void txAnalogicoA0(){
-  char temp[3];                    
+  char temp[3];
   char msg[MSG_LEN] = {0xAD,0x03,0,0,0,0,0}; // set buffer base-value
   int sensorValue = analogRead(sensorPin);   // read A0
+  vw_rx_stop();
+  delayForRadioRxAdj();
   itoa(sensorValue, temp, 10);  // int to char-array
   msg[2]=temp[0];               // overwrite 3rd byte
   msg[3]=temp[1];               // overwrite 4th byte
   msg[4]=temp[2];               // overwrite 5th byte
-  delay(100);                   // delay
   vw_send((uint8_t *)msg,MSG_LEN); // send to tx radio
-  vw_wait_tx();                    // wait until message is gone  
+  vw_wait_tx();                    // wait until message is gone
+  vw_rx_start();
+}
+
+void delayForRadioRxAdj(){
+  // this delay is necessary because TX-KEYBOARD and
+  // RX-DISPLAY are near and TX blind RX.
+  // allows RX to listen regulary
+  delay(500);
 }
