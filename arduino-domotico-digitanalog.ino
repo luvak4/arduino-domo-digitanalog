1f57b451948958e2ec58ff43c8ab5e670a5a726d
luvak4 <luvak4@gmail.com>
2016-10-17 13:39:02 +0200

    Update arduino-domotico-digitanalog.ino

--- arduino-domotico-digitanalog.ino
+++ arduino-domotico-digitanalog.ino
@@ -14,9 +14,14 @@
 #define RELE_TOGGLE 123
 #define READ_DATA   124
 //
+#define VELOCITAstd 500
+//
 int  INTERIlocali[4]={0,0,0,0};
 byte BYTEradio[BYTEStoTX];
-byte CIFR[]={223,205,228,240,43,146,241,87,213,48,235,131,6,81,26,70,34,74,224,27,111,150,22,138,239,200,179,222,231,212};
+byte CIFR[]={223,205,228,240,43,146,241,/
+	     87,213,48,235,131,6,81,26,/
+	     70,34,74,224,27,111,150,22,/
+	     138,239,200,179,222,231,212};
 
 const unsigned long mask=0x0000FFFF;
 //
@@ -32,45 +37,31 @@ unsigned long millPrec=0;
 // setup
 ////////////////////////////////
 void setup() {
-  pinMode(rele_pin, OUTPUT);    // led set pin
-  pinMode(led_pin,OUTPUT);          // out pin 13
-  digitalWrite(led_pin,LOW);        // ... set low  
-  vw_set_tx_pin(transmit_pin); // radio set tx pin
-  vw_set_rx_pin(receive_pin);  // radio set rx pin
-  vw_setup(500);              // radio speed
-  vw_rx_start();               // radio rx ON
+  pinMode(rele_pin, OUTPUT);   
+  pinMode(led_pin,OUTPUT);     
+  digitalWrite(led_pin,LOW);  
+  vw_set_tx_pin(transmit_pin);
+  vw_set_rx_pin(receive_pin); 
+  vw_setup(VELOCITAstd);              
+  vw_rx_start();              
   Serial.begin(9600);
 }
 ////////////////////////////////
 // loop
 ////////////////////////////////
 void loop(){
-  ////////////////////////////////
-  //delay(1000);
-  //txStato();
-  ////////////////////////////////
   if (vw_get_message(BYTEradio, &buflen)){
+    // decifra
     decodeMessage();
     digitalWrite(led_pin,HIGH);
-    //delay(600);
-    
+    // controlal mittente
     if (INTERIlocali[INDIRIZZO]==indirMAESTRO){
+      // esegue comando
       switch (INTERIlocali[DATOa]){
-      case RELE_ON:
-        digitalWrite(rele_pin,HIGH);
-        txStato();
-        break;
-      case RELE_OFF:
-        digitalWrite(rele_pin,LOW);
-        txStato();
-        break;
-      case RELE_TOGGLE:
-        digitalWrite(rele_pin,!digitalRead(rele_pin));
-        txStato();
-        break;
-      case READ_DATA:
-        txStato();
-        break;
+      case RELE_ON:digitalWrite(rele_pin,HIGH);	txStato();break;
+      case RELE_OFF:digitalWrite(rele_pin,LOW);	txStato();break;
+      case RELE_TOGGLE:	digitalWrite(rele_pin,!digitalRead(rele_pin));txStato();break;
+      case READ_DATA:txStato();	break;
       } 
     }
     delay(100);
@@ -78,18 +69,32 @@ void loop(){
   }
 }
 
+int arrotondaTemp(int valore){
+  // valore: per 21,76 deve inserirsi il valore 2176
+  // quindi moltiplicare per cento il valore restituito
+  // come float
+  char c[];
+  itoa(valore,c,10);
+  // gli ultimi due numero sono i decimali
+  byte numCar=sizeof(c);
+  
+}
+
 void txStato(){
+  // imposta il proprio nome come mittente
   INTERIlocali[INDIRIZZO]=indirCANTINA;
+  // recupera valori
   int sensorVal = analogRead(temperPin);
   float voltage = (sensorVal / 1024.0) * 5.0;
   float temperature = (voltage - .5) * 10000;
   int temper=temperature;
-  //Serial.println(temperature);
-    //Serial.println(temper);
+  // valori in memoria
   INTERIlocali[DATOa]=analogRead(lightPin);
   INTERIlocali[DATOb]=temper;
   INTERIlocali[DATOc]=digitalRead(rele_pin) & 0xFF;
+  // codifica in bytes
   encodeMessage();
+  // tx
   vw_rx_stop();
   delayForRadioRxAdj();
   vw_send((uint8_t *)BYTEradio,BYTEStoTX); // send to tx-radio
