fe497e584d73f14e456fc270e37b22ca0c8f8d4f
luvak4 <luvak4@gmail.com>
2016-10-18 13:16:17 +0200

    Update arduino-domotico-digitanalog.ino

--- arduino-domotico-digitanalog.ino
+++ arduino-domotico-digitanalog.ino
@@ -1,154 +1,139 @@
 #include <VirtualWire.h>
-//
-#define INDIRIZZO  0
-#define DATOa      1
-#define DATOb      2
-#define DATOc      3
-//
-#define indirMAESTRO   1234
-#define indirCANTINA 1235
-//
-#define BYTEStoTX  8
+////////////////////////////////
+// pins
+////////////////////////////////
+const int rele_pin     =  2;
+const int led_pin_rx   = 13;
+const int led_pin_tx   = 10;
+const int receive_pin  = 11;
+const int transmit_pin = 12;
+const int lightPin     = A0;
+const int temperPin    = A1;
+////////////////////////////////
+// indirizzi radio
+////////////////////////////////
+#define indirMAESTRO        1234 //rx
+#define indirCANTINA        1235 //tx
+////////////////////////////////
+// comunicazione radio principale
+////////////////////////////////
+#define VELOCITAstd   500
+#define INDIRIZZO       0
+#define DATOa           1
+#define DATOb           2
+#define DATOc           3
+#define BYTEStoTX       8
+int     INTERIlocali[4]={0,0,0,0};
+byte    BYTEradio[BYTEStoTX];
+uint8_t buflen = BYTEStoTX; //for rx
+////////////////////////////////
+// comandi da decodificare
+////////////////////////////////
 #define RELE_ON     121
 #define RELE_OFF    122
 #define RELE_TOGGLE 123
 #define READ_DATA   124
-//
-#define VELOCITAstd 500
-//
-int  INTERIlocali[4]={0,0,0,0};
-byte BYTEradio[BYTEStoTX];
+////////////////////////////////
+// varie
+////////////////////////////////
 byte CIFR[]={223,205,228,240,43,146,241,//
 	     87,213,48,235,131,6,81,26,//
 	     70,34,74,224,27,111,150,22,//
 	     138,239,200,179,222,231,212};
-
 const unsigned long mask=0x0000FFFF;
-//
-const int rele_pin = 2;
-const int led_pin = 13;      // led pin
-const int receive_pin = 11;
-const int transmit_pin = 12;
-uint8_t buflen = BYTEStoTX;    //for rx
-const int lightPin = A0;
-const int temperPin = A1;
-unsigned long millPrec=0;
-// per timer mio
-int dutyCycle = 0;
-unsigned long int Pa;
-unsigned long int Pb;
-byte Minuti=0;
-byte Secondi=0;
-byte numVolte=0;
 ////////////////////////////////
 // setup
 ////////////////////////////////
 void setup() {
-  pinMode(rele_pin, OUTPUT);   
-  pinMode(led_pin,OUTPUT);     
-  digitalWrite(led_pin,LOW);  
+  pinMode(rele_pin, OUTPUT);
+  pinMode(led_pin_rx,OUTPUT);
+  pinMode(led_pin_tx,OUTPUT);  
+  digitalWrite(led_pin_rx,LOW);
+  digitalWrite(led_pin_tx,LOW);
   vw_set_tx_pin(transmit_pin);
   vw_set_rx_pin(receive_pin); 
   vw_setup(VELOCITAstd);              
-  vw_rx_start();              
-  Serial.begin(9600);
+  vw_rx_start();
+  tempo=millis():
+  //Serial.begin(9600); // debug
 }
+#define SALITA  1
+#define DISCESA 0
+unsigned long tempo;
+int  temperVALattuale;
+int  temperVALprecedente;
+bool temperSTAattuale; //1=RAISE 0=FALL
+bool temperSTAprecedente; //1=RAISE 0=FALL
+int  temperMINUTIstato;
+//
+int  luceVALattuale;
+int  luceVALprecedente;
+bool luceSTAattuale; //1=RAISE 0=FALL
+bool luceSTAprecedente; //1=RAISE 0=FALL
+int  luceMINUTIstato;
+//
 ////////////////////////////////
 // loop
 ////////////////////////////////
 void loop(){
-  unsigned long int Qa;
-  unsigned long int Qb;
-  int DIFFa;
-  int DIFFb;
-  int Xa;
-  int Xb;
-  //
-  dutyCycle += 1;
-  if (dutyCycle > 9){
-    dutyCycle = 0;
+  ////////////////////////////////
+  // ogni minuto
+  ////////////////////////////////
+  if ((millis()-tempo)>60000){
+    tempo=millis();
   }
-  if (dutyCycle > 0){
-    Qa=millis();
-    if (Qa >= Pa){
-      DIFFa=Qa-Pa;
-      Xa = DIFFa - 25;
-      if (Xa >= 0){
-        Pa = Qa;
-        //--------------------------------
-        // da qui passa ogni 0.025 Sec
-        // quarto di decimo di secondo
-        //--------------------------------
-        if (vw_get_message(BYTEradio, &buflen)){
-          // decifra
-          decodeMessage();
-          digitalWrite(led_pin,HIGH);
-          // controlal mittente
-          if (INTERIlocali[INDIRIZZO]==indirMAESTRO){
-            // esegue comando
-            switch (INTERIlocali[DATOa]){
-            case RELE_ON:digitalWrite(rele_pin,HIGH);  txStato();break;
-            case RELE_OFF:digitalWrite(rele_pin,LOW); txStato();break;
-            case RELE_TOGGLE: digitalWrite(rele_pin,!digitalRead(rele_pin));txStato();break;
-            case READ_DATA:txStato(); break;
-            } 
-          }
-          delay(100);
-          digitalWrite(led_pin,LOW);
-        }
-        //----------- fine ---------------
+  ////////////////////////////////
+  if (vw_get_message(BYTEradio, &buflen)){
+    vw_rx_stop();
+    // decifra
+    decodeMessage();
+    // controlla mittente
+    switch (INTERIlocali[INDIRIZZO]){
+    case indirMAESTRO:
+      digitalWrite(led_pin_rx,HIGH);
+      // esegue comando
+      switch (INTERIlocali[DATOa]){
+      case RELE_ON:digitalWrite(rele_pin,HIGH); txStato();break;
+      case RELE_OFF:digitalWrite(rele_pin,LOW); txStato();break;
+      case RELE_TOGGLE: digitalWrite(rele_pin,!digitalRead(rele_pin));txStato();break;
+      case READ_DATA:txStato(); break;
       }
-    } else {
-      Pa = Qa - Xa;
+      delay(100);
+      digitalWrite(led_pin_rx,LOW);	    
+      break;
     }
-  } else {  
-    Qb=millis();
-    if (Qb >= Pb){
-      DIFFb=Qb-Pb;
-      Xb = DIFFb - 1000;
-      if (Xb >= 0){
-        Pb = Qb - Xb;
-        //--------------------------------
-        // da qui passa ogni secondo
-        //--------------------------------
-        Secondi++;
-        if (Secondi>59){
-          Secondi=0;
-          Minuti++;
-        }
-        switch (Minuti){
-          case 11:
-          txStato();
-          break;
-          case 12:
-          txStato();
-          break;
-          case 13:
-          txStato();
-          break;
-          case 14:
-          Minuti=0;
-          break;
-          default:
-          break;
-        }
-        //----------- fine ---------------
-      }
-    } else {
-      Pb = Qb;
-    }      
+    vw_rx_start();
   }
 }
 
-int arrotondaTemp(int valore){
-  // valore: per 21,76 deve inserirsi il valore 2176
-  // quindi moltiplicare per cento il valore restituito
-  // come float
-//  char c[];
-//  itoa(valore,c,10);
-  // gli ultimi due numero sono i decimali
-//  byte numCar=sizeof(c);
-  
+void chkTemperatura(){
+  int sensorVal = analogRead(temperPin);
+  float voltage = (sensorVal / 1024.0) * 5.0;
+  float temperature = (voltage - .5) * 10000;
+  temperVALattuale=temperature;
+  int diff=abs(temperVALattuale-temperVALprecedente);
+  if (diff>100){
+    // la differenza
+    // col valore precedente e' consistente
+    if (temperVALattuale > temperVALprecedente){
+      temperSTAattuale=SALITA;
+    } else {
+      if (temperVALattuale < temperVALprecedente){
+	temperSTAattuale=DISCESA;	
+      } else {
+	/// esce
+      }
+    }
+    /////////////////
+    temperVALprecedente=temperVALattuale;
+    if (temperSTAattuale==temperSTAprecedente){
+      temperMINUTIstato++;
+    } else {
+      temperMINUTIstato=0;
+      temperSTAprecedente=temperSTAattuale
+    }
+    /////////////////
+  }
 }
 
 void txStato(){
@@ -166,24 +151,28 @@ void txStato(){
   // codifica in bytes
   encodeMessage();
   // tx
+  //******************************
+  // prima di trasmettere attende 
+  // che l'AGC di rx-MAESTRO
+  // abbia recuperato
+  //******************************
+  delay(1500); // IMPORTANTE 
+  //******************************
   vw_rx_stop();
-  delayForRadioRxAdj();
-  vw_send((uint8_t *)BYTEradio,BYTEStoTX); // send to tx-radio
-  vw_wait_tx();                    // wait until message is gone
+  digitalWrite(led_pin_tx,HIGH);  
+  vw_send((uint8_t *)BYTEradio,BYTEStoTX); 
+  vw_wait_tx();
+  delay(100);
+  digitalWrite(led_pin_tx,LOW);  
   vw_rx_start();
 }
 
-void delayForRadioRxAdj(){
-  // this delay is necessary because TX-KEYBOARD and
-  // RX-DISPLAY are near and TX blind RX.
-  // allows RX to listen regulary
-  delay(1000);
-}
-
+////////////////////////////////
+// RADIO -> locale
+////////////////////////////////
 void decodeMessage(){
-  // from byte to struct
   byte m=0;
-  cipher(); // cifratura
+  cipher();
   for (int n=0; n<4;n++){
     INTERIlocali[n]=BYTEradio[m+1];
     INTERIlocali[n]=INTERIlocali[n] << 8;
@@ -192,8 +181,10 @@ void decodeMessage(){
   }
 }
 
+////////////////////////////////
+// locale -> RADIO
+////////////////////////////////
 void encodeMessage(){
-  // from struct to byte
   byte m=0;
   for (int n=0; n<4;n++){
     BYTEradio[m]=INTERIlocali[n] & mask;
@@ -201,9 +192,12 @@ void encodeMessage(){
     BYTEradio[m+1]=INTERIlocali[n] & mask;
     m+=2;
   }
-  cipher(); // cifratura
+  cipher();
 }
 
+////////////////////////////////
+// cifratura XOR del messaggio
+////////////////////////////////
 void cipher(){
   for (byte n=0;n<8;n++){
     BYTEradio[n]=BYTEradio[n]^CIFR[n];
